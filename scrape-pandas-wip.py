8a347d9ba685eb75a08d94e099b867bc1917ea55
Adrian Chrysanthou <adrianvfx@gmail.com>
2018-09-10 13:31:07 -0400

    Pandas WIP Python
    included pandas wip modification to the python scrape script

--- scrape-pandas-wip.py
+++ scrape-pandas-wip.py
@@ -3,6 +3,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 import pymysql
 import csv
 from itertools import islice
+from datetime import datetime
+import pandas as pd
 conn = pymysql.connect(host = "127.0.0.1",
                        user = "root",
                        passwd = "",
@@ -26,7 +28,7 @@ gc = gspread.authorize(credentials)
 
 wks = gc.open("Departures and Nominations").sheet1
 #worksheet = wks.worksheet('Copy of Resignations and Nominations')
-cell_list = wks.range('A4:L141')
+cell_list = wks.range('A4:Q641')
 
 for i, worksheet in enumerate(cell_list):
     filename = '-worksheet' + '.csv'
@@ -41,19 +43,38 @@ cursor.execute("SELECT id, Name, Department, Position, DatesHired, DatesLeft, To
 #rows = cursor.fetchall()
 cursor.execute('Truncate `mooches_table` ')
 #clean and remove top row
+
+#test = pd.read_csv("-worksheet.csv", encoding='latin-1', parse_dates=[4], dayfirst=True)
+
+#print (test)
+dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m-%d')
+
+df = pd.read_csv("-worksheet.csv", encoding='latin-1', error_bad_lines=False, warn_bad_lines=False, parse_dates=[4], date_parser=dateparse)
+#df = pd.read_csv("-worksheet.csv", encoding='latin-1', error_bad_lines=False,
+
+
+# Changing datatype
+df['Date Hired'] = pd.to_datetime(df['Date Hired'])
 with open("-worksheet.csv", "rt") as infile, open("cleaned.csv", "w") as outfile:
    reader = csv.reader(infile)
+
    writer = csv.writer(outfile)
+   # for column in islice(reader, 4, None):
+   #     print(column[4])
    for row in islice(reader, 4, None):
        # process each row
        #writer.writerow(row)
-        #new_row = [' '.join([row[0], row[1]])]
-        help = [' '.join([row[0], row[1]])], row[2].strip(),row[3].strip(), row[4].strip(), row[6].strip(), row[7].strip(), row[8].strip(), row[9].strip(), row[10].strip(), row[11].strip(), row[16].strip()
-        #print (new_row)
-        print (help)
-        sql = "INSERT INTO mooches_table(Name, Department, Position, DatesHired, DatesLeft, TotalTimes, TimesunderTrump, TimesMooches, FiredResign, Notes, Img) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
-        cursor.execute(sql, help)
 
-conn.commit()
-cursor.close()
+        #new_row = [' '.join([row[0], row[1]])]
+
+
+        #print(row[])
+        #help = [' '.join([row[0], row[1]])], row[2].strip(),row[3].strip(), row[4].strip(), row[6].strip(), row[7].strip(), row[8].strip(), row[9].strip(), row[10].strip(), row[11].strip(), row[16].strip()
+        #print (new_row)
+        #print (help)
+        #sql = "INSERT INTO mooches_table(Name, Department, Position, DatesHired, DatesLeft, TotalTimes, TimesunderTrump, TimesMooches, FiredResign, Notes, Img) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+        #cursor.execute(sql, help)
+
+        conn.commit()
+        cursor.close()
 print ("Done")
