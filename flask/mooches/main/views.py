d1c237565498aa6e76ffc6381bd43ea78148fa7d
tinyzimmer <38474291+tinyzimmer@users.noreply.github.com>
2018-09-17 14:17:54 -0700

    latest changes

--- flask/mooches/main/views.py
+++ flask/mooches/main/views.py
@@ -2,7 +2,13 @@ from datetime import datetime
 from flask import render_template
 
 from . import main
+from .. import models
 
 @main.route("/")
 def index():
     return render_template("index.html")
+
+@main.route("/data", methods=["GET", "POST"])
+def data():
+    # db query and JSON return go here
+    return "None"
