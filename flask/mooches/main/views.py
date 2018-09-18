099290a5179488af9ecfaf8d668f11e63d3ae851
tinyzimmer <38474291+tinyzimmer@users.noreply.github.com>
2018-09-17 18:32:11 -0700

    add working datatable return

--- flask/mooches/main/views.py
+++ flask/mooches/main/views.py
@@ -1,14 +1,42 @@
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request
+from datatables import DataTable
+import json
 
 from . import main
-from .. import models
+from .. import models, db
+
+
+TRUMP_INAUGURAL = datetime.strptime("01/20/2017", "%m/%d/%Y").date()
+
+
+def trumpTime(startDate, leaveDate):
+    if startDate < TRUMP_INAUGURAL:
+        startDate = TRUMP_INAUGURAL
+    return (leaveDate - startDate).days
+
 
 @main.route("/")
 def index():
     return render_template("index.html")
 
-@main.route("/data", methods=["GET", "POST"])
+@main.route("/data", methods=["POST"])
 def data():
-    # db query and JSON return go here
-    return "None"
+    table = DataTable(
+        request.form,
+        models.Mooch,
+        db.session.query(models.Mooch),
+        [
+            ("Name", "LastName", lambda i: "{}, {}".format(i.LastName, i.FirstName)),
+            ("Affiliation"),
+            ("Position"),
+            ("Hired", "DateHired", lambda i: "{}".format(i.DateHired.strftime("%m/%d/%Y"))),
+            ("Left", "DateLeft", lambda i: "{}".format(i.DateLeft.strftime("%m/%d/%Y"))),
+            ("Total Days", lambda i: "{}".format((i.DateLeft - i.DateHired).days)),
+            ("Under Trump", lambda i: "{}".format(trumpTime(i.DateHired, i.DateLeft))),
+            ("Mooches", "MoochesTime"),
+            ("Fired/Resign", "LeaveType"),
+            ("Notes")
+        ]
+    )
+    return json.dumps(table.json())
