8426228c0553f11b012599ba284e8782dd49b09a
tinyzimmer <38474291+tinyzimmer@users.noreply.github.com>
2018-09-17 18:56:03 -0700

    add scraper thread

--- flask/mooches/__init__.py
+++ flask/mooches/__init__.py
@@ -1,4 +1,6 @@
 import os
+import atexit
+import threading
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,14 +8,45 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 db = SQLAlchemy()
 
-def bootstrap_app():
+from . import models
+
+updateThread = threading.Thread()
+
+def bootstrap_app(no_thread=False):
     app = Flask(__name__)
+
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % (
         os.path.join(basedir, "data.sqlite")
     )
     app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
     db.init_app(app)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    def interrupt():
+        global updateThread
+        print("Stopping updates")
+        updateThread.cancel()
+
+    def runUpdate():
+        global updateThread
+        print("Running mooch update")
+        with app.app_context():
+            models.update()
+        updateThread = threading.Timer(10, runUpdate, ())
+        updateThread.start()
+
+    def startUpdates():
+        global updateThread
+        print("Starting update thread")
+        updateThread = threading.Timer(10, runUpdate, ())
+        updateThread.start()
+
+    if not no_thread:
+        startUpdates()
+        atexit.register(interrupt)
+
     return app
