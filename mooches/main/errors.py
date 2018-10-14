deb56f4dca006996d1d9cd76f16c2fddd1915683
tinyzimmer <38474291+tinyzimmer@users.noreply.github.com>
2018-10-13 17:36:22 -0700

    wrap position, fix unknown image

--- mooches/main/errors.py
+++ mooches/main/errors.py
@@ -1,8 +1,10 @@
-from flask import render_template
+from flask import render_template, request, send_file
 from . import main
 
 @main.app_errorhandler(404)
 def page_not_found(e):
+    if request.path.endswith(".JPG"):
+        return send_file("static/img/unknown.JPG")
     return render_template('404.html'), 404
 
 @main.app_errorhandler(500)
